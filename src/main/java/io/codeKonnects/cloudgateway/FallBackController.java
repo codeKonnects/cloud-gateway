f8a0cef6dbf5cd0ae7d9efdbb8191f989167b8d8
codeKonnects <topedaves@gmail.co>
2022-10-25 01:29:34 +0100

    implement cloud gateway for microservice APIs

---
+++ src/main/java/io/codeKonnects/cloudgateway/FallBackController.java
@@ -0,0 +1,19 @@
+package io.codeKonnects.cloudgateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackController {
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod() {
+        return "User service is taking longer than expected." +
+                "Please try again later";
+    }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod() {
+        return "Department service is taking longer than expected." +
+                "Please try again later";
+    }
+}
